df67b597eee9e9ef4fd50aa516471c2152561b7d
Ivan Kucherov <kucher_up@mail.ru>
2023-09-23 20:41:39 +0300

    Финальные правки - Лента Новостей

--- src/js/files/net-to-slider.js
+++ src/js/files/net-to-slider.js
@@ -1,63 +1,9 @@
-// === NET to SLIDER === //
-
-// const slider = document.querySelector('.publications-slider');
-
-// let publicationsSwiper = null;
-
-// function mobileSlider() {
-//   const MOBILE_BREAKPOINT = 949.98;
-//   if (window.innerWidth <= MOBILE_BREAKPOINT && slider.dataset.mobile === 'false') {
-// 	  publicationsSwiper = new Swiper(slider, {
-// 		  slidesPerView: 'auto',
-// 		  spaceBetween: 20,
-// 		  observer: true,
-// 		  observeParents: true,
-// 		  observeSlideChildren: true,
-// 		  navigation: {
-// 			  prevEl: '.publications-slider__nav-prev',
-// 			  nextEl: '.publications-slider__nav-next',
-// 		  },
-// 		  breakpoints: {
-// 			  320: {
-// 				  slidesPerView: 1,
-// 			  },
-// 			  649.98: {
-// 				  slidesPerView: 2,
-// 			  },
-// 			  [MOBILE_BREAKPOINT]: {
-// 				  slidesPerView: 'auto',
-// 			  },
-// 		  }
-// 	  });
-
-//     slider.dataset.mobile = 'true';
-//   }
-
-//   if (window.innerWidth > MOBILE_BREAKPOINT) {
-//     slider.dataset.mobile = 'false';
-
-//     if (slider.classList.contains('swiper-container-initialized')) {
-//       publicationsSwiper.destroy();
-//     }
-//   }
-// }
-
-// mobileSlider();
-
-// window.addEventListener('load', () => {
-// 	mobileSlider();
-// });
-// const resizeObserver = new ResizeObserver(mobileSlider);
-// resizeObserver.observe(document.documentElement);
-
-
 const slider = document.querySelector('.publications-slider');
-
+const MOBILE_BREAKPOINT = 991.98;
 let publicationsSwiper = null;
 
 function initSwiper() {
-	const MOBILE_BREAKPOINT = 767.98;
-	if (window.innerWidth <= MOBILE_BREAKPOINT && slider && slider.dataset.mobile === 'false') {
+	if (window.innerWidth <= MOBILE_BREAKPOINT && slider) {
 		publicationsSwiper = new Swiper(slider, {
 			slidesPerView: "auto",
 			spaceBetween: 10,
@@ -81,11 +27,8 @@ function initSwiper() {
 				},
 			}
 		});
-
-		slider.dataset.mobile = 'true';
 	} else if (publicationsSwiper) {
 		publicationsSwiper.destroy();
-		slider.dataset.mobile = 'false';
 	}
 }
 
